2c30f98653cc74a03494bb11c2021dc0db006e03
aleksandrgrigorev <xundr1992@gmail.com>
2023-04-10 17:34:57 +0400

    Changed "Today"/popular to "publishedAt"

--- app/src/main/java/com/grigorev/and3/Api.kt
+++ app/src/main/java/com/grigorev/and3/Api.kt
@@ -7,12 +7,11 @@ import retrofit2.http.Query
 
 const val BASE_URL = "https://newsapi.org/v2/"
 const val API_KEY = "17e5c12b76344b1fa54a7fb5ec177150"
-const val DATE = "today"
 const val NEWS_NUMBER = "20"
 
 interface Api {
 
-    @GET("everything?from=$DATE&sortBy=popularity&pageSize=$NEWS_NUMBER&apiKey=$API_KEY")
+    @GET("everything?sortBy=publishedAt&pageSize=$NEWS_NUMBER&apiKey=$API_KEY")
     suspend fun getNews(@Query("q") category: String): NewsApiResponse
 
     companion object {
